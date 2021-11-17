3ad4a419ef34083e1c35bfe82e757de808fed871
AssafKlein <assafklein27@gmail.com>
2021-11-17 20:26:59 +0200

    try2

--- hw1/README.md
+++ hw1/README.md
@@ -1,1 +1,2 @@
 # ComputerArch
+git test
---
+++ run.sh
@@ -0,0 +1,27 @@
+gppd11 bp_main.c bp.cpp 
+./a.out tests/example1.trc > cpp_out/out1 ; diff cpp_out/out1 ref_results/example1.out 
+./a.out tests/example2.trc > cpp_out/out2 ; diff cpp_out/out2 ref_results/example2.out 
+./a.out tests/example3.trc > cpp_out/out3 ; diff cpp_out/out3 ref_results/example3.out 
+./a.out tests/example4.trc > cpp_out/out4 ; diff cpp_out/out4 ref_results/example4.out 
+./a.out tests/example5.trc > cpp_out/out5 ; diff cpp_out/out5 ref_results/example5.out 
+./a.out tests/example6.trc > cpp_out/out6 ; diff cpp_out/out6 ref_results/example6.out 
+./a.out tests/example7.trc > cpp_out/out7 ; diff cpp_out/out7 ref_results/example7.out 
+./a.out tests/example8.trc > cpp_out/out8 ; diff cpp_out/out8 ref_results/example8.out 
+./a.out tests/example9.trc > cpp_out/out9 ; diff cpp_out/out9 ref_results/example9.out 
+./a.out tests/example10.trc > cpp_out/out10 ; diff cpp_out/out10 ref_results/example10.out 
+./a.out tests/example11.trc > cpp_out/out11 ; diff cpp_out/out11 ref_results/example11.out 
+./a.out tests/example12.trc > cpp_out/out12 ; diff cpp_out/out12 ref_results/example12.out 
+./a.out tests/example13.trc > cpp_out/out13 ; diff cpp_out/out13 ref_results/example13.out 
+./a.out tests/example14.trc > cpp_out/out14 ; diff cpp_out/out14 ref_results/example14.out 
+./a.out tests/example15.trc > cpp_out/out15 ; diff cpp_out/out15 ref_results/example15.out 
+./a.out tests/example16.trc > cpp_out/out16 ; diff cpp_out/out16 ref_results/example16.out 
+./a.out tests/example17.trc > cpp_out/out17 ; diff cpp_out/out17 ref_results/example17.out 
+./a.out tests/example18.trc > cpp_out/out18 ; diff cpp_out/out18 ref_results/example18.out 
+./a.out tests/example19.trc > cpp_out/out19 ; diff cpp_out/out19 ref_results/example19.out 
+./a.out tests/example20.trc > cpp_out/out20 ; diff cpp_out/out20 ref_results/example20.out
+
+./a.out NEW_TESTS/example1.trc > cpp_out/out1 ; diff cpp_out/out1 NEW_TESTS/example1.out 
+./a.out NEW_TESTS/example2.trc > cpp_out/out2 ; diff cpp_out/out2 NEW_TESTS/example2.out 
+./a.out NEW_TESTS/example3.trc > cpp_out/out3 ; diff cpp_out/out3 NEW_TESTS/example3.out 
+./a.out NEW_TESTS/example4.trc > cpp_out/out4 ; diff cpp_out/out4 NEW_TESTS/example4.out 
+./a.out NEW_TESTS/example5.trc > cpp_out/out5 ; diff cpp_out/out5 NEW_TESTS/example5.out 
